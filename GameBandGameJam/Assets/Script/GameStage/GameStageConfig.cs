0576c095b25f4f795e794a98e520aae892ae4f06
Tony <>
2026-07-27 23:04:06 +0800

    import naughty attributes/ add valid to stage config

--- GameBandGameJam/Assets/Script/GameStage/GameStageConfig.cs
+++ GameBandGameJam/Assets/Script/GameStage/GameStageConfig.cs
@@ -1,9 +1,40 @@
 using UnityEditor;
 using UnityEngine;
+using NaughtyAttributes;
 
 [CreateAssetMenu(fileName = "GameStageConfig", 
    menuName = "Configs/GameStageConfig")]
 public class GameStageConfig : ScriptableObject
 {
    public EnumDictionary<GameStageType , SceneAsset> gameStages = new EnumDictionary<GameStageType , SceneAsset>();
+
+   [Button]
+   void OnValidate()
+   {
+      foreach (var gameStage in gameStages)
+      {
+         var sceneAsset = gameStage.Value;
+         if (sceneAsset == null)
+            continue;
+
+         var scenePath = AssetDatabase.GetAssetPath(sceneAsset);
+         var isEnabledInBuild = false;
+
+         foreach (var buildScene in EditorBuildSettings.scenes)
+         {
+            if (buildScene.path == scenePath && buildScene.enabled)
+            {
+               isEnabledInBuild = true;
+               break;
+            }
+         }
+
+         if (!isEnabledInBuild)
+         {
+            Debug.LogWarning(
+               $"Scene '{sceneAsset.name}' assigned to {gameStage.Key} is not enabled in Build Settings.",
+               this);
+         }
+      }
+   }
 }
